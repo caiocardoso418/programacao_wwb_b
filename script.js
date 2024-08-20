f7d2d164222c8830cce109366972671fe8f0ac16
caiocardoso418 <caiocardoso418@gmail.com>
2024-08-20 09:39:04 -0300

    aula 20/08

--- script.js
+++ script.js
@@ -34,9 +34,33 @@ function validaCPF(cpf) {
     }
 
     if(cpf.length != 11 && cpf.length != 14){
-        alert("Formato inválido")
-        return false
+        alert("Formato inválido");
+        return false;
     }
+
+    let soma = 0;
+
+    for(let i=1; i<=9;i++){
+        soma = soma + (cpf.charAt(i-1) * (10 -(i-1)));
+    }
+
+    let resto = soma % 11;
+    
+   if(resto < 2){
+        if(cpf.charAt(9) != 0 ){
+            alert("CPF Inválido!");
+            return false;
+        }
+        return true;
+   }    
+
+   let digitoverificador1 = 11 - resto;
+   if(digitoverificador1 != cpf.charAt(9)){
+    alert("CPF Inválido!");
+    return false;
+
+   }
+
 
     return true;
 }
